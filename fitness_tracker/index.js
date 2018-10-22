b921748937de9b535aca13e09a3fe0bc6a6b59ce
RiCK BROWN <ricky.brown.00@gmail.com>
2018-10-22 22:00:45 +0100

    working model

---
+++ fitness_tracker/index.js
@@ -0,0 +1,50 @@
+// DOM elements
+const btns = document.querySelectorAll('button')
+const form = document.querySelector('form')
+const formActivity = document.querySelector('form span')
+const input = document.querySelector('input')
+const error = document.querySelector('.error')
+
+// Event Listeners
+let activity = 'cycling'
+
+btns.forEach(btn => {
+  btn.addEventListener('click', e => {
+    // Get activity (from HTML lines 32-35 'data-activity')
+    activity = e.target.dataset.activity
+
+    // Remove and add active class
+    btns.forEach(btn => btn.classList.remove('active'))
+    e.target.classList.add('active')
+
+    // Set the id of input field
+    input.setAttribute('id', activity)
+
+    // Set the text of form span
+    formActivity.textContent = activity
+
+    // Call the update function
+    update(data)
+  })
+})
+
+// Form submit
+form.addEventListener('submit', e => {
+  // Prevent the default 'submit' event (page refresh)
+  e.preventDefault()
+
+  const distance = parseInt(input.value)
+  if(distance) {
+    db.collection('activities').add({
+      distance,
+      activity,
+      date: new Date().toString()
+    }).then(() => {
+      error.textContent = ''
+      input.value = ''
+    })
+  } else {
+    error.textContent = 'Please enter a valid distance!'
+    input.value = ''
+  }
+})
---
+++ sandbox/e.js
@@ -0,0 +1,16 @@
+// create dotted line group and append to graph
+const dottedLines = graph.append('g')
+  .attr('class', 'lines')
+  .style('opacity', 0);
+
+// create x dotted line and append to dotted line group
+const xDottedLine = dottedLines.append('line')
+  .attr('stroke', '#aaa')
+  .attr('stroke-width', 1)
+  .attr('stroke-dasharray', 4);
+
+// create y dotted line and append to dotted line group
+const yDottedLine = dottedLines.append('line')
+  .attr('stroke', '#aaa')
+  .attr('stroke-width', 1)
+  .attr('stroke-dasharray', 4);
